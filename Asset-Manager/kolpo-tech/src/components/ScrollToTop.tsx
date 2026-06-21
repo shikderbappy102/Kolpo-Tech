34eeced970ce2a6ab7da7ff3eba0baf344fe4b04
shikderbappy102 <shikderbappy102@gmail.com>
2026-06-21 21:44:39 +0600

    ScrollToTop.tsx

---
+++ Asset-Manager/kolpo-tech/src/components/ScrollToTop.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useLocation } from "wouter";
+
+export default function ScrollToTop() {
+  const [location] = useLocation();
+
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "auto"
+      });
+    });
+  }, [location]);
+
+  return null;
+}
